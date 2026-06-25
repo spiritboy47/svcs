721c58e1f11d1a896f68531998cb34d7e5d46cc5
NIKHIL KUMAR A <113880516+spiritboy47@users.noreply.github.com>
2026-06-25 13:12:04 +0530

    Update pages.py

--- app/routes/pages.py
+++ app/routes/pages.py
@@ -2,35 +2,68 @@ from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
 
 router = APIRouter()
+
 templates = Jinja2Templates(directory="app/templates")
+
 
 @router.get("/")
 def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html",
+        context={}
+    )
+
 
 @router.get("/about")
 def about(request: Request):
-    return templates.TemplateResponse("about.html", {"request": request})
+    return templates.TemplateResponse(
+        request=request,
+        name="about.html",
+        context={}
+    )
+
 
 @router.get("/services")
 def services(request: Request):
-    return templates.TemplateResponse("services.html", {"request": request})
+    return templates.TemplateResponse(
+        request=request,
+        name="services.html",
+        context={}
+    )
+
 
 @router.get("/menu")
 def menu(request: Request):
-    return templates.TemplateResponse("menu.html", {"request": request})
+    return templates.TemplateResponse(
+        request=request,
+        name="menu.html",
+        context={}
+    )
+
 
 @router.get("/gallery")
 def gallery(request: Request):
-    return templates.TemplateResponse("gallery.html", {"request": request})
+    return templates.TemplateResponse(
+        request=request,
+        name="gallery.html",
+        context={}
+    )
+
 
 @router.get("/alumni")
 def alumni(request: Request):
-    return templates.TemplateResponse("alumni.html", {"request": request})
+    return templates.TemplateResponse(
+        request=request,
+        name="alumni.html",
+        context={}
+    )
+
 
 @router.get("/contact")
 def contact(request: Request):
     return templates.TemplateResponse(
-        "contact.html",
-        {"request": request}
+        request=request,
+        name="contact.html",
+        context={}
     )
